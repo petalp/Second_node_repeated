9ef68ad3010f19881209499d8597c8f6aab169ce
petalp <peteralpha09@gmail.com>
2025-05-04 18:04:00 +0000

    updates files

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -7,7 +7,7 @@ import db from '../db.js'
 const router = express.Router()
 
 //Register a new user endpoint /auth/register
- router.post('/register', (req, res)=>{
+router.post('/register', (req, res)=>{
     const { username, password } = req.body;
     // save the username and irreversibly encrypted password
     // save jamestoma@gmail.com | rtelknimsmfgnsfgf
@@ -26,7 +26,7 @@ const router = express.Router()
         insertTodo.run(result.lastInsertRowid, defaultTodo);
 
         //create a token
-        const token = jwt.sign({ id: result.lastInsertRowid }, process.env.JWT_SECRET,{ expiresIn:'2s' });
+        const token = jwt.sign({ id: result.lastInsertRowid }, process.env.JWT_SECRET,{ expiresIn:'24h'});
         
         res.json({token })
     }catch(err){
@@ -36,14 +36,23 @@ const router = express.Router()
 
  })
 
- router.post('/login', (req, res)=>{
+router.post('/login', (req, res)=>{
         const {username, password} = req.body
 
         try{
             const getUser = db.prepare(`SELECT * FROM users WHERE username=?`);
             const user = getUser.get(username);
 
-            if(!user){return res.status(404).send({message:"user not found"})}
+            // check if user exist
+            if(!user){return res.status(404).send({message:"user not found"})};
+
+            const passwordIsValid = bcrypt.compareSync(password, user.password);
+            // check if password is valid 
+            if(!passwordIsValid){return res.status(404).send({message : "Invalid password"})}
+
+            // console.log(user);
+            const token = jwt.sign({id: user.id},process.env.JWT_SECRET,{ expiresIn:'24h'} )
+            res.json({token})
         }catch(err){
             console.log(err.message);
             res.sendStatus(503);
